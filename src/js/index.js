ab2cd9b187b187ea85bf8ca94de281f751830f2c
Allan Machado <124932917+allandmachado@users.noreply.github.com>
2023-02-11 18:57:39 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+};
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+
+})
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
